420533234e85d98faa95526489c384afb28023f4
Andriy13 <alomaga@i.ua>
2015-12-01 20:17:04 +0200

    Andriy13 1 commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CreditCard cc = new CreditCard();
         }
     }
 }
---
+++ lab/CreditCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    class CreditCard
+    {
+        int balance;
+        public CreditCard()
+        {
+            Random r = new Random();
+            balance = r.Next();
+            Console.WriteLine(balance);
+        }
+    }
+}
